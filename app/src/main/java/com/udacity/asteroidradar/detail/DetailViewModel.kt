8a3beb9bb20b8399394f8c053aff0c40596c0f0e
alik13 <ali.kh1615@gmail.com>
2022-08-14 15:18:47 +0200

    Initial commit

---
+++ app/src/main/java/com/udacity/asteroidradar/detail/DetailViewModel.kt
@@ -0,0 +1,9 @@
+package com.udacity.asteroidradar.detail
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DetailViewModel (application: Application): AndroidViewModel(application)
+{
+
+}
