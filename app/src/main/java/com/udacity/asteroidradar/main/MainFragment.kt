f2f753ead68c6b52df3a667a9bcae2941b7e1aaa
alik13 <ali.kh1615@gmail.com>
2022-08-19 16:00:24 +0200

    Initialize recycler view adapter and navigate.

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -1,9 +1,17 @@
 package com.udacity.asteroidradar.main
 
+import android.accounts.AccountManager.get
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
+import com.udacity.asteroidradar.AsteroidsAdapter
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
@@ -19,6 +27,25 @@ class MainFragment : Fragment() {
         binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
+
+        binding.asteroidRecycler.adapter = AsteroidsAdapter(AsteroidsAdapter.OnClickListener{
+            viewModel.displayAsteroidDetails(it)
+        })
+
+//        viewModel.picture.observe(viewLifecycleOwner, Observer {
+//
+//            if(it.mediaType == "image") {
+//                Glide.with(this).load(it.url).into(binding.activityMainImageOfTheDay)
+//            }
+//        })
+
+        viewModel.navigateToSelectedAsteroid.observe(viewLifecycleOwner, Observer {
+            if(it != null)
+            {
+                this.findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
+                viewModel.displayAsteroidDetailsComplete()
+            }
+        })
 
         setHasOptionsMenu(true)
 
