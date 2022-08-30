6882dbb6adf635d798009c05642fc5d1ffddd479
alik13 <ali.kh1615@gmail.com>
2022-08-30 19:35:10 +0200

    Add class for creating the database.

--- app/src/main/java/com/udacity/asteroidradar/database/Database.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/Database.kt
@@ -1,2 +1,33 @@
 package com.udacity.asteroidradar.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [DatabaseAsteroid::class,DatabasePictureOfDay::class], version = 1)
+abstract class AsteroidRadarDatabase: RoomDatabase()
+{
+    abstract val asteroidRadarDao: AsteroidRadarDao
+
+    companion object {
+        @Volatile
+        private var Instance: AsteroidRadarDatabase? = null
+
+        fun getInstance(context:Context): AsteroidRadarDatabase
+        {
+            synchronized(this)
+            {
+                var instance = Instance
+                if(instance == null)
+                {
+                    instance = Room.databaseBuilder(context.applicationContext,AsteroidRadarDatabase::class.java,"asteroids_db").fallbackToDestructiveMigration().build()
+                    Instance = instance
+                }
+                return instance
+            }
+        }
+
+
+    }
+}
