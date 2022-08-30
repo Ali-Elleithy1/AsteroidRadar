3404ee2b3943765f0e9c051422a62ce26d062a46
alik13 <ali.kh1615@gmail.com>
2022-08-30 19:36:29 +0200

    Add class for the network objects.

---
+++ app/src/main/java/com/udacity/asteroidradar/api/DataTransferObjects.kt
@@ -0,0 +1,53 @@
+package com.udacity.asteroidradar.api
+
+import androidx.lifecycle.Transformations.map
+import com.squareup.moshi.JsonClass
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.PictureOfDay
+import com.udacity.asteroidradar.database.DatabaseAsteroid
+
+
+@JsonClass(generateAdapter = true)
+data class NetworkAstroidContainer(val asteroids: List<NetworkAsteroid>)
+
+@JsonClass(generateAdapter = true)
+data class NetworkAsteroid(
+    val id: Long,
+    val codename: String,
+    val closeApproachDate: String,
+    val absoluteMagnitude: Double,
+    val estimatedDiameter: Double,
+    val relativeVelocity: Double,
+    val distanceFromEarth: Double,
+    val isPotentiallyHazardous: Boolean
+)
+
+fun NetworkAstroidContainer.asDomainModel(): List<Asteroid> {
+    return asteroids.map {
+        Asteroid(
+            id = it.id,
+            codename = it.codename,
+            closeApproachDate = it.closeApproachDate,
+            absoluteMagnitude = it.absoluteMagnitude,
+            estimatedDiameter = it.estimatedDiameter,
+            relativeVelocity = it.relativeVelocity,
+            distanceFromEarth = it.distanceFromEarth,
+            isPotentiallyHazardous = it.isPotentiallyHazardous
+        )
+    }
+}
+
+fun NetworkAstroidContainer.asDatabaseModel(): Array<DatabaseAsteroid> {
+    return asteroids.map {
+        DatabaseAsteroid(
+            id = it.id,
+            codename = it.codename,
+            closeApproachDate = it.closeApproachDate,
+            absoluteMagnitude = it.absoluteMagnitude,
+            estimatedDiameter = it.estimatedDiameter,
+            relativeVelocity = it.relativeVelocity,
+            distanceFromEarth = it.distanceFromEarth,
+            isPotentiallyHazardous = it.isPotentiallyHazardous
+        )
+    }.toTypedArray()
+}
