67e7fd5c3ae1a1e364b638e202f2deed2846b523
alik13 <ali.kh1615@gmail.com>
2022-08-30 21:29:59 +0200

    Adjust the return.

--- app/src/main/java/com/udacity/asteroidradar/database/DatabaseDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/DatabaseDao.kt
@@ -10,19 +10,19 @@ import java.util.ArrayList
 @Dao
 interface AsteroidRadarDao {
     @Query("SELECT * FROM databaseasteroid ORDER BY closeApproachDate ASC")
-    fun getAsteroids(): LiveData<List<DatabaseAsteroid>>
+    fun getAsteroids(): LiveData<List<DatabaseAsteroid>?>
 
     @Query("SELECT * FROM databaseasteroid WHERE closeApproachDate = :today")
-    fun getTodayAsteroids(today: String): LiveData<List<DatabaseAsteroid>>
+    fun getTodayAsteroids(today: String): LiveData<List<DatabaseAsteroid>?>
 
     @Query("SELECT * FROM databaseasteroid WHERE closeApproachDate IN (:sevenDaysDates) ORDER BY closeApproachDate ASC")
-    fun getWeekAsteroids(sevenDaysDates: ArrayList<String>): LiveData<List<DatabaseAsteroid>>
+    fun getWeekAsteroids(sevenDaysDates: ArrayList<String>): LiveData<List<DatabaseAsteroid>?>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(asteroids: List<DatabaseAsteroid>)
 
     @Query("select * from databasepictureofday")
-    fun getPicture(): LiveData<DatabasePictureOfDay>
+    fun getPicture(): LiveData<DatabasePictureOfDay?>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertPicture(picture: DatabasePictureOfDay)
